e2d7565600de79753b6deb6b8345080e244fad21
Ying Dong <dongyingname@yahoo.com>
2018-10-30 22:11:11 -0600

    Changed the look

--- express_server.js
+++ express_server.js
@@ -8,7 +8,9 @@ var PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 //start engine bodyParser
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 // Generate 6 digit string
 function generateRandomString() {
     var str = '';
@@ -23,43 +25,60 @@ function generateRandomString() {
 //Our Database
 var urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
-    "9sm5xK": "http://www.google.com"
+    "9sm5xK": "http://www.google.com",
+    "8sc9m1": "http://www.youtube.com"
 };
 
 //Route to print out urls in /urls 
 app.get("/urls", (req, res) => {
-    let templateVars = { urls: urlDatabase };
+    let templateVars = {
+        urls: urlDatabase
+    };
     res.render("urls_index", templateVars);
-  });
+});
+
 //Route for post
 app.get("/urls/new", (req, res) => {
     res.render("urls_new");
-  });
+});
+
+//Route to redirect
+app.get("/u/:shortURL", (req, res) => {
+    let longURL = urlDatabase[req.params.shortURL];
+    res.redirect(longURL);
+});
+
 //Route for test  
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // debug statement to see POST parameters
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+    console.log(req.body); // debug statement to see POST parameters
+    res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
-  
+
 //Second route
 app.get("/urls/:id", (req, res) => {
     let longURL = urlDatabase[req.params.id];
     console.log(longURL);
-    let templateVars = { shortURL:req.params.id, longURL };
+    let templateVars = {
+        shortURL: req.params.id,
+        longURL
+    };
     res.render("urls_show", templateVars);
-  });  
+});
+
 //main page
 app.get("/", (req, res) => {
     res.send("Hello!");
 });
+
 //display full URL
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
 });
+
 //hello page
 app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
-  });
+});
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`);
